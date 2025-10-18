1a1ed76ef904ca23126fb771d36b67474b38ac0c
Sannidhi.Sunkad <01fe24ica004@kletech.ac.in>
2025-10-18 19:45:31 +0530

    sannidhis journal arithematics

---
+++ arithematics.py
@@ -0,0 +1,7 @@
+# add and subract two numbers
+num1=int (input("Enter first number: "))
+num2=int (input("Enter second number: "))
+sum=num1+num2
+diff=num1-num2
+print("Sum of two numbers:",sum)
+print("Difference of two numbers:",diff)
